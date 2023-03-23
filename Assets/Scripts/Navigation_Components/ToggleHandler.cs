05da826360ca8f10b34fb4ef291d4fc507c15350
LongAtGitHub <ltruong@macalester.edu>
2023-03-23 16:30:33 -0500

    Fix toggler

--- Assets/Scripts/Navigation_Components/ToggleHandler.cs
+++ Assets/Scripts/Navigation_Components/ToggleHandler.cs
@@ -10,17 +10,27 @@ public class ToggleHandler : MonoBehaviour
     public Vector3 openPosition  = new Vector3(0f,423f, 0f);
     public Vector3 closePosition = new Vector3(0f, -409f, 0f);
     // Start is called before the first frame update
+    public bool isOpen = false;
     void Start()
     {
         
         RectTransform rt = panel.GetComponent<RectTransform>();
         rt.anchoredPosition = closePosition;
     }
+    public void changePanelState(){
+        if (isOpen){
+            close();
+        }else{
+            open();
+        }
+    }
     public void open(){
         panel.GetComponent<RectTransform>().anchoredPosition = openPosition;
+        isOpen = true;
     }
     public void close(){
         panel.GetComponent<RectTransform>().anchoredPosition = closePosition;
+        isOpen = false;
     }
 
     // Update is called once per frame
